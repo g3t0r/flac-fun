3a2fd16e045128d15f66a3fced580ac09aa28217
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2023-06-24 23:06:36 +0200

    Created super simple callbacks

--- src/main.c
+++ src/main.c
@@ -6,6 +6,18 @@
 #include "string.h"
 #include "FLAC/stream_decoder.h"
 
+FLAC__StreamDecoderWriteStatus writeCallback(const FLAC__StreamDecoder *decoder, const FLAC__Frame *frame, const FLAC__int32 *const buffer[], void *client_data) {
+  printf("Hello from write callback :)\n");
+  return 0;
+}
+
+void metadataCallback(const FLAC__StreamDecoder *decoder, const FLAC__StreamMetadata *metadata, void *client_data) {
+  printf("Hello from metadata callback");
+}
+
+void errorCallback(const FLAC__StreamDecoder *decoder, FLAC__StreamDecoderErrorStatus status, void *client_data) {
+  printf("Hello from error callback");
+}
 
 int main(int argc, char **argv) {
   FILE *fd = fopen("audio/1.flac", "rb");
@@ -15,6 +27,6 @@ int main(int argc, char **argv) {
   }
 
   FLAC__StreamDecoder * decoder = FLAC__stream_decoder_new();
-  FLAC__StreamDecoderInitStatus status = FLAC__stream_decoder_init_FILE(decoder, fd, NULL, NULL, NULL, NULL);
+  FLAC__StreamDecoderInitStatus status = FLAC__stream_decoder_init_FILE(decoder, fd, writeCallback, metadataCallback, errorCallback, NULL);
   printf("StreamDecoderStatus: %d\n", status);
 }
