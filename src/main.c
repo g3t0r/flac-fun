eda7d15fd96853de16c32418268be22ae4a61626
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2023-06-25 14:28:34 +0200

    reformatted main.c

--- src/main.c
+++ src/main.c
@@ -2,19 +2,23 @@
 #include <stdint.h>
 #define __STDC_FORMAT_MACROS
 
+#include "FLAC/stream_decoder.h"
 #include "inttypes.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-#include "FLAC/stream_decoder.h"
 
-FLAC__StreamDecoderWriteStatus writeCallback(const FLAC__StreamDecoder *decoder, const FLAC__Frame *frame, const FLAC__int32 *const buffer[], void *client_data) {
-  //printf("Frame number: %u\n", frame->header.number.frame_number);
-  //printf("Channel assignment: %u\n", frame->header.channel_assignment );
+FLAC__StreamDecoderWriteStatus writeCallback(const FLAC__StreamDecoder *decoder,
+                                             const FLAC__Frame *frame,
+                                             const FLAC__int32 *const buffer[],
+                                             void *client_data) {
+  // printf("Frame number: %u\n", frame->header.number.frame_number);
+  // printf("Channel assignment: %u\n", frame->header.channel_assignment );
   return 0;
 }
 
-void metadataCallback(const FLAC__StreamDecoder *decoder, const FLAC__StreamMetadata *metadata, void *client_data) {
+void metadataCallback(const FLAC__StreamDecoder *decoder,
+                      const FLAC__StreamMetadata *metadata, void *client_data) {
   printf("Hello from metadata callback\n");
   printf("\tMetadataType: %u\n", metadata->type);
   printf("\tMin blocksize: %u\n", metadata->data.stream_info.min_blocksize);
@@ -25,7 +29,8 @@ void metadataCallback(const FLAC__StreamDecoder *decoder, const FLAC__StreamMeta
   printf("\tMetadataType: %u\n", metadata->type);
 }
 
-void errorCallback(const FLAC__StreamDecoder *decoder, FLAC__StreamDecoderErrorStatus status, void *client_data) {
+void errorCallback(const FLAC__StreamDecoder *decoder,
+                   FLAC__StreamDecoderErrorStatus status, void *client_data) {
   printf("Hello from error callback");
 }
 
@@ -36,13 +41,14 @@ int main(int argc, char **argv) {
     exit(1);
   }
 
-  FLAC__StreamDecoder * decoder = FLAC__stream_decoder_new();
-  FLAC__StreamDecoderInitStatus status = FLAC__stream_decoder_init_FILE(decoder, fd, writeCallback, metadataCallback, errorCallback, NULL);
+  FLAC__StreamDecoder *decoder = FLAC__stream_decoder_new();
+  FLAC__StreamDecoderInitStatus status = FLAC__stream_decoder_init_FILE(
+      decoder, fd, writeCallback, metadataCallback, errorCallback, NULL);
 
-FLAC__bool ok =  FLAC__stream_decoder_process_until_end_of_stream(decoder);
-if(!ok) {
-  printf("Something is not OK\n");
-}
+  FLAC__bool ok = FLAC__stream_decoder_process_until_end_of_stream(decoder);
+  if (!ok) {
+    printf("Something is not OK\n");
+  }
 
   uint32_t blockSize = FLAC__stream_decoder_get_blocksize(decoder);
   uint32_t rate = FLAC__stream_decoder_get_sample_rate(decoder);
