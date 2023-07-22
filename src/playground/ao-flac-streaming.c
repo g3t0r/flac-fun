08c5806d3de75f3783c76c90eea46ae62b19b0f9
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2023-07-22 13:23:14 +0200

    IT WORKS IT WORKS IT WORKS IT WORKS!!!!

--- src/playground/ao-flac-streaming.c
+++ src/playground/ao-flac-streaming.c
@@ -2,6 +2,7 @@
 #include <FLAC/ordinals.h>
 #include <FLAC/stream_decoder.h>
 #include <ao/ao.h>
+#include <math.h>
 #include <semaphore.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -32,39 +33,67 @@ FLAC__StreamDecoderReadStatus readCb(const FLAC__StreamDecoder *decoder,
     sem_getvalue(&semaphore, &semValue);
     printf("Semaphore value: %d\n", semValue);
   }
-  printf("decoder tries to read %lu bytes\n", *bytes);
+  // printf("decoder tries to read %lu bytes\n", *bytes);
   size_t readBytes = fread(buffer, sizeof(char), *bytes, fd);
   *bytes = readBytes;
-  printf("provided %lu bytes\n", readBytes);
+  // printf("provided %lu bytes\n", readBytes);
 
   return FLAC__STREAM_DECODER_READ_STATUS_CONTINUE;
 }
 
 FLAC__StreamDecoderWriteStatus writeCb(const FLAC__StreamDecoder *decoder,
                                        const FLAC__Frame *frame,
-                                       const int *const *buffer,
+                                       const FLAC__int32 *const *buffer,
                                        void *clientData) {
 
-  printf("decoder tries to write %u bytes\n", frame->header.blocksize);
-  format.byte_format = AO_FMT_BIG;
-  format.channels = frame->header.channels;
-  format.bits = frame->header.bits_per_sample;
+  uint32_t blocksize = frame->header.blocksize; // number of samples
+  uint32_t bytesPerSample = 3;
+  uint32_t numberOfChannels = 2;
+  uint32_t rate = 44100;
 
-  uint32_t blocksize = frame->header.blocksize;
-  uint32_t bytesPerSample = format.bits / 8;
-  uint32_t numberOfChannels = format.channels;
+  uint32_t bufferSize = blocksize * bytesPerSample * numberOfChannels;
+  printf("Buffer size: %u\n", bufferSize);
 
-  char * tmpBuffer = malloc(blocksize);
+  uint32_t mask = 0xFF; // full byte
 
-  for(int i = 0; i < blocksize; i+= bytesPerSample) {
-    int channel = (i / bytesPerSample) % numberOfChannels;
-    memcpy(tmpBuffer + i, const void *, unsigned long)
+  char *tmpBuffer = calloc(bufferSize, sizeof(char));
+
+  for (int i = 0; i < blocksize; i++) {
+
+    int leftChannel = i * bytesPerSample * numberOfChannels;
+    int rightChannel = i * bytesPerSample * numberOfChannels + bytesPerSample;
+
+    tmpBuffer[leftChannel] = (int8_t)(buffer[0][i] >> 16) & mask;
+    tmpBuffer[rightChannel] = (int8_t)(buffer[1][i] >> 16) & mask;
+    tmpBuffer[leftChannel + 1] = (int8_t)(buffer[0][i] >> 8) & mask;
+    tmpBuffer[rightChannel + 1] = (int8_t)(buffer[1][i] >> 8) & mask;
+    tmpBuffer[leftChannel + 2] = (int8_t)buffer[0][i] & mask;
+    tmpBuffer[rightChannel + 2] = (int8_t)buffer[1][i] & mask;
   }
 
+  // buf_size = format.bits / 8 * format.channels * format.rate;
+  // char *tmpBuffer = calloc(buf_size, sizeof(char));
 
+  // buf_size = format.bits / 8 * format.channels * format.rate;
+  // buffer = calloc(buf_size, sizeof(char));
 
+  // int freq = 432;
+  // for (int i = 0; i < format.rate; i++) {
+  //   sample =
+  //       (int)(0.75 * 32768.0 * sin(2 * M_PI * freq * ((float)i /
+  //       format.rate)));
 
+  //   /* Put the same stuff in left and right channel */
+  //   tmpBuffer[4 * i] = tmpBuffer[4 * i + 2] = sample & 0xff;
+  //   tmpBuffer[4 * i + 1] = tmpBuffer[4 * i + 3] = (sample >> 8) & 0xff;
+  // }
 
+  // ao_play(device, tmpBuffer, buf_size);
+
+  int playR = ao_play(device, tmpBuffer, bufferSize);
+  free(tmpBuffer);
+  printf("Errno: %d\n", errno);
+  printf(strerror(errno));
 
   return FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE;
 }
@@ -75,7 +104,19 @@ void errorCb(const FLAC__StreamDecoder *decoder,
 int main() {
   ao_initialize();
   default_driver = ao_default_driver_id();
+
+  printf("default driver: %d\n", default_driver);
   memset(&format, 0, sizeof(format));
+  format.byte_format = AO_FMT_BIG;
+  format.channels = 2;
+  format.bits = 24;
+  format.rate = 44100;
+
+  ao_option option = {"verbose"};
+  device = ao_open_live(default_driver, &format, &option);
+  if (device == NULL) {
+    printf("Device == null\n");
+  }
 
   sem_init(&semaphore, 0, 5);
 
