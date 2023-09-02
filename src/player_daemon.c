0bcd176424f4ee511fc6013e01386781c8a29fa0
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2023-09-03 01:43:13 +0200

    Fixed errors added durring disabled lsp

--- src/player_daemon.c
+++ src/player_daemon.c
@@ -25,13 +25,14 @@ enum PlayerDaemonAudioStatus {
 
 struct PlayerDaemon {
   enum PlayerDaemonAudioStatus player_status;
+  struct Playback * playback;
   struct DataMessage message_buffer[FFUN_PLAYBACK_DAEMON_DATA_MESSAGE_SERIES_SIZE];
   uint message_series_element_counter;
   sem_t message_series_element_counter_mutex;
   sem_t message_series_data_merge_mutex;
   struct {
     int socket;
-    struct sockaddr sock_addr;
+    struct sockaddr_in sock_addr;
   } content_server;
 };
 
@@ -44,8 +45,8 @@ int main(int argc, char** argv) {
 
   player_daemon.player_status = PLAYER_DAEMON_AUDIO_STATUS_PLAYING;
   player_daemon.message_series_element_counter = 0;
-  sem_init(player_daemon.message_series_element_counter_mutex, 0, 1);
-  sem_init(player_daemon.message_series_data_merge_mutex, 0, 0);
+  sem_init(&player_daemon.message_series_element_counter_mutex, 0, 1);
+  sem_init(&player_daemon.message_series_data_merge_mutex, 0, 0);
 
   player_daemon.content_server.socket = socket(AF_INET, SOCK_DGRAM, 0);
   player_daemon.content_server.sock_addr.sin_family = AF_INET;
@@ -55,7 +56,7 @@ int main(int argc, char** argv) {
 
 
   struct pollfd poll_fd;
-  poll_fd.fd = socket;
+  poll_fd.fd = player_daemon.content_server.socket;
   poll_fd.events = POLLIN;
   char udp_data_buffer[FFUN_UDP_DGRAM_MAX_SIZE];
 
@@ -83,7 +84,7 @@ int main(int argc, char** argv) {
     size_t header_size = deserializeMessageHeader(udp_data_buffer, header);
     deserializeDataMessage(udp_data_buffer + header_size, data_message);
 
-    player_daemon_handle_data_message(header, data_message);
+    player_daemon_handle_data_message(&player_daemon, header, data_message);
   }
 
   return 0;
@@ -94,7 +95,7 @@ void player_daemon_handle_data_message(
     struct MessageHeader * message_header,
     struct DataMessage * data_message) {
 
-  player_daemon->message_buffer[message_header->seq] = data_message;
+  player_daemon->message_buffer[message_header->seq] = *data_message;
   player_daemon->message_series_element_counter++;
 
   if(player_daemon->message_series_element_counter != 5) {
@@ -112,39 +113,39 @@ void * player_daemon_request_data_loop_thread_fn(struct PlayerDaemon player_daem
   };
 
   char udp_data_buffer[FFUN_UDP_DGRAM_MAX_SIZE];
-  size_t udp_data_size =  serializeMessageHeader(&header udp_data_buffer);
+  size_t udp_data_size =  serializeMessageHeader(&message_header, udp_data_buffer);
   udp_data_size += serializeFeedMeMessage(&feed_me_message,
-      udp_data_buffer + header_size);
+      udp_data_buffer + udp_data_size);
 
   while(player_daemon.player_status == PLAYER_DAEMON_AUDIO_STATUS_PLAYING) {
     
-    sendto(player_daemon->content_server.socket,
+    sendto(player_daemon.content_server.socket,
         udp_data_buffer,
         udp_data_size,
         0,
-        player_daemon->content_server.sock_addr,
-        sizeof(player_daemon->content_server.sock_addr));
+        (struct sockaddr *) &player_daemon.content_server.sock_addr,
+        sizeof(player_daemon.content_server.sock_addr));
 
     struct timespec time_spec;
     clock_gettime(CLOCK_REALTIME, &time_spec);
-    time_spec.tv += 1000000 /* nsec in milisec*/
-      * FFUN_PLAYBACK_DAEMON_DATA_MESSAGE_SERIES_TIMEOUT_MS
-    sem_timedwait(player_deamon->message_series_data_merge_mutex, &time_spec);
+    time_spec.tv_nsec += 1000000 /* nsec in milisec*/
+      * FFUN_PLAYBACK_DAEMON_DATA_MESSAGE_SERIES_TIMEOUT_MS;
+    sem_timedwait(&player_daemon.message_series_data_merge_mutex, &time_spec);
 
     char message_series_merge_buffer[FFUN_PLAYBACK_DAEMON_DATA_MESSAGE_SERIES_SIZE
       * FFUN_UDP_DGRAM_MAX_SIZE];
 
     int merged_data_size = 0;
-    for(int i = 0; i < player_daemon->message_series_element_counter; i++) {
+    for(int i = 0; i < player_daemon.message_series_element_counter; i++) {
       memcpy(message_series_merge_buffer + merged_data_size,
-          (player_daemon->message_buffer + i)->data,
-          (player_daemon->message_buffer + i)->dataSize);
+          (player_daemon.message_buffer + i)->data,
+          (player_daemon.message_buffer + i)->dataSize);
 
-      merged_data_size += (player_daemon->messages_data_buffer + i)->dataSize;
+      merged_data_size += (player_daemon.message_buffer + i)->dataSize;
     }
 
     playback_feed_data(
-        player_daemon->playback,
+        player_daemon.playback,
         message_series_merge_buffer,
         merged_data_size);
   }
