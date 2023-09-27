6626a1ef4d2309d771fa7b94195c26bcd92d8bff
Jan Jakubowski <jan.jakubowski.ti@gmail.com>
2023-09-28 00:12:57 +0200

    Music library generation raw example

--- src/server.c
+++ src/server.c
@@ -1,10 +1,12 @@
 #include "server.h"
 #include "config.h"
 #include "messages.h"
+#include "logs.h"
 #include <arpa/inet.h>
 #include <asm-generic/socket.h>
 #include <assert.h>
 #include <bits/pthreadtypes.h>
+#include <dirent.h>
 #include <errno.h>
 #include <net/if.h>
 #include <netinet/in.h>
@@ -21,6 +23,41 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+struct AlbumEntry {
+  size_t first_song_id;
+  int album_size;
+  char name[256];
+};
+
+struct Albums {
+  size_t size;
+  struct AlbumEntry * items;
+};
+
+struct SongEntry {
+  size_t album_id;
+  char name[256];
+};
+
+struct Songs {
+  size_t size;
+  struct SongEntry * items;
+};
+
+struct Library {
+  struct Albums * album_list;
+  struct Songs * song_list;
+};
+
+
+struct Library * library_init();
+
+struct Albums * library_albums();
+
+void library_album_songs(struct Library * library, size_t album_id);
+
+const char MUSIC_LIBRARY_PATH[] = "/tmp/music";
+
 static void initializeServer(struct ServerContext *serverContext);
 
 static int handleFeedMeMessage(struct ServerContext *serverContext,
@@ -31,6 +68,9 @@ FILE *debugFile;
 
 int main(int argc, char **argv) {
 
+  struct Library * library = library_init();
+  library_album_songs(library, 0);
+  return 0;
   struct ServerContext serverContext;
   initializeServer(&serverContext);
   printf("sd: %u\n", serverContext.socket);
@@ -205,4 +245,112 @@ static int handleFeedMeMessage(struct ServerContext *serverContext,
   }
 
   return 0;
+}
+
+struct Library * library_init() {
+  DIR * music_dir = opendir(MUSIC_LIBRARY_PATH);
+  size_t music_dir_len = strlen(MUSIC_LIBRARY_PATH);
+  char path_buffer[3*265];
+
+  memcpy(path_buffer, MUSIC_LIBRARY_PATH, music_dir_len);
+  path_buffer[music_dir_len] = '/';
+
+  if(music_dir == NULL) {
+    print_error("Error on opening music library dir: %s\n", strerror(errno));
+  }
+
+  struct Library * library = malloc(sizeof *library);
+  struct Albums * album_list = malloc(sizeof *album_list);
+  struct Songs * song_list = malloc(sizeof *song_list);
+  album_list->size = 10;
+  album_list->items = malloc(10 * sizeof *album_list->items);
+  song_list->size = 20;
+  song_list->items = malloc(20 * sizeof *song_list->items);
+
+  struct dirent * library_dirent;
+  struct dirent * album_dirent;
+  errno = 0;
+  size_t album_count = 0;
+  size_t song_count = 0;
+  while((library_dirent = readdir(music_dir))) {
+
+    if(library_dirent->d_name[0] == '.') {
+      continue;
+    }
+
+    album_list->items[album_count].first_song_id = song_count;
+
+    int album_name_len = strlen(library_dirent->d_name);
+
+    if(album_count != 0 && album_list->size == album_count) {
+      album_list->size <<= 2;
+      album_list->items =
+        realloc(album_list->items, album_list->size * sizeof *album_list->items);
+    }
+
+    memcpy(path_buffer + music_dir_len + 1, library_dirent->d_name, 256);
+    DIR * album_dir = opendir(path_buffer);
+    path_buffer[music_dir_len + 1 + album_name_len] = '/';
+
+    while((album_dirent = readdir(album_dir))) {
+      int song_name_len = strlen(album_dirent->d_name);
+      if(strncmp(album_dirent->d_name + song_name_len - 4, "flac", 4)) {
+        continue;
+      }
+
+      if(song_count != 0 && song_list->size == song_count) {
+        song_list->size <<= 2;
+        song_list->items =
+          realloc(song_list->items, song_list->size * sizeof *song_list->items);
+      }
+
+      song_list->items[song_count].album_id = album_count;
+      memcpy(song_list->items[song_count].name, album_dirent->d_name, 256);
+
+      song_count++;
+    }
+
+    album_list->items[album_count].album_size
+      = song_count - album_list->items[album_count].first_song_id;
+
+    if(!album_list->items[album_count].album_size) {
+      continue;
+    }
+
+    memcpy(album_list->items[album_count].name, library_dirent->d_name, 256);
+
+    if(album_dir == NULL) {
+      print_error("Error on opening album dir: %s\n", strerror(errno));
+    }
+
+    closedir(album_dir);
+    album_count++;
+
+  }
+
+  album_list->size = album_count;
+  album_list->items = realloc(album_list->items,
+                              album_count* sizeof *album_list->items);
+
+  song_list->size = song_count;
+  song_list->items = realloc(song_list->items,
+                              song_count* sizeof *song_list->items);
+
+  if(errno != 0) {
+    print_error("Error while iterating directory: %s\n", strerror(errno));
+  }
+
+  library->album_list = album_list;
+  library->song_list = song_list;
+
+  return library;
+}
+
+void library_album_songs(struct Library * library, size_t album_id) {
+  struct AlbumEntry * album = library->album_list->items + album_id;
+  for(int i = 0; i < album->album_size; i++) {
+    struct SongEntry * song = library->song_list->items + album->first_song_id + i;
+    print_debug("%s\n", song->name);
+  }
+
 }
